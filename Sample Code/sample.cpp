c73012b316b678155c047280ca7a6cf4b344ee3f
tanvirs27 <rifat.csedu20@gmail.com>
2015-10-23 17:11:24 +0600

    sample code

---
+++ Sample Code/sample.cpp
@@ -0,0 +1,47 @@
+#include<cstdio>
+#include<sstream>
+#include<cstdlib>
+#include<cctype>
+#include<cmath>
+#include<algorithm>
+#include<set>
+#include<queue>
+#include<stack>
+#include<list>
+#include<iostream>
+#include<fstream>
+#include<numeric>
+#include<string>
+#include<vector>
+#include<cstring>
+#include<map>
+#include<iterator>
+using namespace std;
+
+#define ll long long int
+#define db double
+#define mem(a,b) memset((a),b,sizeof(a))
+#define READ freopen("c:\\Users\\Rifat\\Desktop\\in.txt", "r", stdin)
+#define sf(n)       scanf("%d", &n)
+#define sff(a,b)    scanf("%d%d", &a, &b)
+#define sfff(a,b,c) scanf("%d%d%d", &a, &b, &c)
+#define pf printf
+#define gc getchar()
+#define DBG(a) printf("in %d\n",a)
+#define all(V) V.begin(),V.end()
+#define max(a,b) (a>b) ? (a):(b)
+#define min(a,b) (a<b) ? (a):(b)
+#define inf         ((1 << 30) - 1)
+#define eps         1e-9
+#define MOD         100007
+#define MAX         200
+
+
+int main()
+{
+    READ;
+
+
+    return 0;
+}
+
